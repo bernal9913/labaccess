1da708fcae6403f9af289b518d4addfa9affa505
berns <ramirez.bernal.jesus1@gmail.com>
2024-06-20 02:54:17 -0700

    unos peditos con las rutas arrobaEstefy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-//import logo from './logo.svg';
-import logo from './ESCUDO-COLOR.png'
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import EntryForm from "./components/EntryForm";
-import ExitForm from "./components/ExitForm";
-import Home from "./components/home";
-import React from "react";
+import { db } from './firebase';
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EntryForm from './components/EntryForm';
+import ExitForm from './components/ExitForm';
+import Home from './components/home';
 
 function App() {
+    const [entries, setEntries] = useState([]);
+
+    const fetchEntries = async () => {
+        console.log('Fetching entries...');
+        const q = query(collection(db, "entries"), where("dentro", "==", true));
+        try {
+            const querySnapshot = await getDocs(q);
+            const entriesArray = [];
+            querySnapshot.forEach((doc) => {
+                entriesArray.push({ id: doc.id, ...doc.data() });
+            });
+            setEntries(entriesArray);
+            console.log('Entries fetched:', entriesArray);
+        } catch (error) {
+            console.error('Error fetching entries:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchEntries();
+    }, []);
+
     return (
         <Router>
             <div className="App">
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/entryform" element={<EntryForm />} />
-                    <Route path="/exitform" element={<ExitForm />} />
+                    <Route path="/Registro" element={<EntryForm />} />
+                    <Route path="/Salidas" element={<ExitForm entries={entries} fetchEntries={fetchEntries} />} />
                 </Routes>
             </div>
         </Router>
--- src/index.js
+++ src/index.js
@@ -2,28 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter, RouterProvider }  from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import EntryForm from './components/EntryForm';
+import ExitForm from './components/ExitForm';
 import Home from './components/home';
 
 const router = createBrowserRouter([
-  {path: "/",
-    element: <Home />
-  },
-  {path: "/Registro",
-    element: <EntryForm />
-  }
-]
+    { path: "/", element: <Home /> },
+    { path: "/Registro", element: <EntryForm /> },
+    { path: "/Salidas", element: <ExitForm /> }
+]);
 
-)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//
